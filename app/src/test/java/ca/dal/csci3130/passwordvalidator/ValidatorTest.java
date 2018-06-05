8f70d5fe8b6d7a5f14fdc276b200a317fd1bfc70
Alexlinsy <alexlinsy@live.com>
2018-06-05 18:44:59 -0300

    Add more test

--- app/src/test/java/ca/dal/csci3130/passwordvalidator/ValidatorTest.java
+++ app/src/test/java/ca/dal/csci3130/passwordvalidator/ValidatorTest.java
@@ -9,13 +9,27 @@ public class ValidatorTest {
 
     @Test
     public void passwordValidate() {
-        //Create a test password
-        String passwordStage2 = "Lsy123456.";
-        int output;
+        //Create test passwords
+        String passwordStage21 = "lsyyuiuide";
+        String passwordStage22 = "lsy123456";
+        String passwordStage23 = "Lsy123456";
+        String passwordStage24 = "Lsy123456.";
 
-        output = isValid.passwordValidateQ2(passwordStage2);
+        int output;
+        int output2;
+        int output3;
+        int output4;
+
+        output2 = isValid.passwordValidateQ2(passwordStage21);
+        output3 = isValid.passwordValidateQ2(passwordStage22);
+        output4 = isValid.passwordValidateQ2(passwordStage23);
+        output = isValid.passwordValidateQ2(passwordStage24);
         //Test if the string matches requirements
+        assertEquals(2,output2);
+        assertEquals(3,output3);
+        assertEquals(4,output4);
         assertEquals(5,output);
+
 
     }
 }
