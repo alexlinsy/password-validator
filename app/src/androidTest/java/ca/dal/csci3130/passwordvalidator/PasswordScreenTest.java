7884c284eccbb75428b1c064cfe7fb6fafb92981
Alexlinsy <alexlinsy@live.com>
2018-06-05 23:23:33 -0300

    Fix bugs

--- app/src/androidTest/java/ca/dal/csci3130/passwordvalidator/PasswordScreenTest.java
+++ app/src/androidTest/java/ca/dal/csci3130/passwordvalidator/PasswordScreenTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -29,11 +30,13 @@ public class PasswordScreenTest {
 
         //Test a not strong password
         onView(withId(R.id.passwordText)).perform(typeText("lsy123456"));
+        onView(withId(R.id.passwordText)).perform(closeSoftKeyboard());
         onView(withId(R.id.validateButton)).perform(click());
         onView(withId(R.id.resultTextView)).check(matches(withText("The password is not strong")));
 
         //Test a strong password
         onView(withId(R.id.passwordText)).perform(clearText(), typeText("Lsy123456."));
+        onView(withId(R.id.passwordText)).perform(closeSoftKeyboard());
         onView(withId(R.id.validateButton)).perform(click());
         onView(withId(R.id.resultTextView)).check(matches(withText("The password is strong")));
 
